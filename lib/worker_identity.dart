3bff5edb4d2f44b3216275f01a94e10f05de7417
hongxiang <gohuhx@163.com>
2023-01-30 13:51:21 +0800

    [huhx] -feat: add vscode setting files

--- lib/worker_identity.dart
+++ lib/worker_identity.dart
@@ -3,7 +3,7 @@ class WorkerIdentity {
 
   int getWorkerId(String ipAddress) {
     final List<String> ips = ipAddress.split(".");
-    
+
     assert(ips.length > 4, "$ipAddress should be ip v4 address.");
     final int subnet = int.parse(ips[2]);
     final int machine = int.parse(ips[3]);
