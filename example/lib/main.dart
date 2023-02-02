f88ab34f990186d407cde8143ee52022a2111f58
hongxiang <gohuhx@163.com>
2023-02-02 13:45:54 +0800

    [huhx] -feat: remvoe flutter dependencies

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_snowflake/flutter_snowflake.dart';
 
 void main() {
   final int id = Snowflake(3, 4).getId();
-  if (kDebugMode) {
-    print("Generated id $id");
-  }
+  print("Generated id $id");
 }
